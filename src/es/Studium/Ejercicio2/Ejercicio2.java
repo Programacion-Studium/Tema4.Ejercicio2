aeca4427318af1a62d44088f24547e6eff4ab7d2
Alvca <Alvca@DESKTOP-H3LD864>
2020-01-29 10:01:02 +0100

    1

--- src/es/Studium/Ejercicio2/Ejercicio2.java
+++ src/es/Studium/Ejercicio2/Ejercicio2.java
@@ -1,30 +1,31 @@
 package es.Studium.Ejercicio2;
 import java.awt.*;
+import java.awt.event.*;
 /**
  * @author alvaro
  *
  */
-public class Ejercicio2 extends Frame
+public class Ejercicio2 extends Frame  implements WindowListener, ActionListener
 {
 	private static final long serialVersionUID = 1L; 
 	Label lblCantidad = new Label("Introduzca la cantidad"); //Etiqueta que queremos poner.
 	TextField txtCantidad = new TextField(20);//Tamaño visual del campo, ojo no es el tamaño de los datos.
 	Label lblPorcentaje = new Label("Introduzca el porcentaje");
-	TextField txtPorcentaje = new TextField(20);//Tamaño visual del campo, ojo no es el tamaño de los datos.
+	TextField txtPorcentaje = new TextField(20);//Tamaño visual del campo, ojo no es el tamaÃ±o de los datos.
 	Button Calcular = new Button("Calcular");
 	Label lblResultado = new Label("Resultado");
-	TextField txtResultado = new TextField(20);//Tamaño visual del campo, ojo no es el tamaño de los datos.
+	TextField txtResultado = new TextField(20);//Tamaño visual del campo, ojo no es el tamaÃ±o de los datos.
 	//Creamos el panel
 	Panel pnlPanel1 = new Panel();
 	Panel pnlPanel2 = new Panel();
 	Panel pnlPanel3 = new Panel();
 	Panel pnlPanel4 = new Panel();
 
-	public Ejercicio2() 
+	Ejercicio2() 
 	{
-		// Establecer la distribución con 3 filas y 2 columnas 
+		// Establecer la distribución con 3 filas y 2 columnas 
 		setLayout( new GridLayout( 4,2 ) );
-		//añadimos los botones dentro del panel
+		//aÃ±adimos los botones dentro del panel
 		pnlPanel1.add(lblCantidad);
 		pnlPanel1.add(txtCantidad);
 		pnlPanel2.add(lblPorcentaje); 
@@ -36,7 +37,9 @@ public class Ejercicio2 extends Frame
 		add(pnlPanel2);
 		add(pnlPanel3);
 		add(pnlPanel4);
-		setSize(400,200); 
+		setSize(400,200);
+		this.addWindowListener(this);
+		Calcular.addActionListener(this);
 		setLocationRelativeTo(null); 
 		setVisible(true);
 	}
@@ -44,4 +47,47 @@ public class Ejercicio2 extends Frame
 	public static void main(String[] args) {
 		new Ejercicio2(); }
 	// Fin del main 
+	public void actionPerformed(ActionEvent ae) {
+		// TODO Auto-generated method stub
+		Object a=ae.getSource();
+		if (a.equals(Calcular)) {
+			float resultado = Float.parseFloat(txtCantidad.getText())* Float.parseFloat(txtPorcentaje.getText())/(float)100.0;
+			txtResultado.setText(resultado+"");
+		}
+	}
+	@Override
+	public void windowOpened(WindowEvent we) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void windowClosing(WindowEvent we) {
+		
+			System.exit(0); 
+	}
+	@Override
+	public void windowClosed(WindowEvent we) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void windowIconified(WindowEvent we) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void windowDeiconified(WindowEvent we) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void windowActivated(WindowEvent we) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void windowDeactivated(WindowEvent we) {
+		// TODO Auto-generated method stub
+		
+	}
 }
